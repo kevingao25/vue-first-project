3b24caabde8e2f8a20dc7de5653e2e27995d9d8d
Kevin Gao <kevinmingjiang@gmail.com>
2021-06-02 15:55:49 +0800

    fda

---
+++ src/components/Tasks.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-for="(task, index) in tasks" :key="index">
+        <Task :task="task" />
+    </div>
+</template>
+
+<script>
+import Task from './Task'
+
+export default {
+    name: 'Tasks',
+    props: {
+        tasks: Array
+    },
+    component: {
+        Task,
+    }
+}
+</script>
