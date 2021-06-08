ff3e862c4bd0116e4c52a7aaa881c44c034f4333
Kevin Gao <kevinmingjiang@gmail.com>
2021-06-08 23:56:32 +0800

    deploy test

--- server.js
+++ server.js
@@ -1,7 +1,25 @@
-const express = require("express");
-const serveStatic = require("serve-static");
-const path = require("path");
-app = express();
-app.use(serveStatic(path.join(__dirname, "dist")));
-const port = process.env.PORT || 80;
-app.listen(port);
+// const express = require("express");
+// const serveStatic = require("serve-static");
+// const path = require("path");
+// app = express();
+// app.use(serveStatic(path.join(__dirname, "dist")));
+// const port = process.env.PORT || 80;
+// app.listen(port);
+
+const jsonServer = require("json-server");
+const server = jsonServer.create();
+const router = jsonServer.router("./db.json");
+const middlewares = jsonServer.defaults({
+  static: "./build",
+});
+const PORT = process.env.PORT || 5000;
+server.use(middlewares);
+server.use(
+  jsonServer.rewriter({
+    "/api/*": "/$1",
+  })
+);
+server.use(router);
+server.listen(PORT, () => {
+  console.log("Server is running");
+});
