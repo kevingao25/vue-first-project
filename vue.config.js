5d2c70819b6a3f2217f0f407b9c7489d5a1197cc
Kevin Gao <kevinmingjiang@gmail.com>
2021-06-08 21:50:24 +0800

    deploy the webpage

--- vue.config.js
+++ vue.config.js
@@ -1,15 +1,16 @@
 module.exports = {
-    devServer: {
-      proxy: {
-        '^/api': {
-          target: 'http://localhost:5000',
-          changeOrigin: true,
-          logLevel: 'debug',
-          pathRewrite: { '^/api': '/' },
-        },
+  publicPath: process.env.NODE_ENV === "production" ? "/hello-world/" : "/",
+  devServer: {
+    proxy: {
+      "^/api": {
+        target: "http://localhost:5000",
+        changeOrigin: true,
+        logLevel: "debug",
+        pathRewrite: { "^/api": "/" },
       },
     },
-    configureWebpack: {
-      devtool: 'source-map'
-    }
-  }
+  },
+  configureWebpack: {
+    devtool: "source-map",
+  },
+};
